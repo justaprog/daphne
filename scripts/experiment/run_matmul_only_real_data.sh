ce60743542ccc5b764dc6cc6940bebbc28fdd4ca
Duong <trieuduongdc@gmail.com>
2026-02-22 19:56:36 +0100

    Add new scripts and update existing ones for matrix multiplication experiments
    - Created new result files for matrix multiplication experiments with real data:
      - run_matmul_only_real_data.txt
      - run_matmul_self_transpose.txt
    - Updated run_matmul_w.txt with additional results and improved formatting.
    - Added new script run_elementwise_mul.sh to execute element-wise multiplication experiments across various datasets.
    - Modified run_matmul_only_real_data.sh to include additional datasets and improved execution permissions.
    - Created run_matmul_self_transpose.sh for self-transpose matrix multiplication experiments.
    - Updated run_write_mnc_md.sh to include more datasets and improved formatting.

--- scripts/experiment/run_matmul_only_real_data.sh
+++ scripts/experiment/run_matmul_only_real_data.sh
@@ -1,1 +1,20 @@
-bin/daphne --explain property_inference scripts/experiment/matmul_cases.daph M1_WITHOUT_MNC=\"./data/real/bp__1000.mtx\" M1_WITH_MNC=\"./data/real/withmnc/bp__1000.csv\" M1_O=\"./data/real/withmnc/bp__1000.csv\" M2_WITHOUT_MNC=\"./data/real/bp__1200.mtx\" M2_WITH_MNC=\"./data/real/withmnc/bp__1200.csv\" M2_O=\"./data/real/withmnc/bp__1200.csv\"
+# run: chmod +x scripts/experiment/run_matmul_only_real_data.sh
+# run: scripts/experiment/run_matmul_only_real_data.sh
+# all datasets: bcsstk02 (66,66), dwt__66 (66,66), 
+# bp__1000 (822,822), bp__1200 (822,822), 
+# G10 (800,800), G11 (800,800), G14(800,800), 
+# G22 (2000,2000), G27 (2000,2000), 
+# gre__115 (115,115), football__115 (115,115),
+# lp_scagr7 (129, 185), gre__185 (185,185), 
+# gre__512 (512,512), dw256A (512,512), 
+# str__200 (363,363), str__400 (363,363)
+bin/daphne scripts/experiment/matmul_w.daph M1_WITHOUT_MNC=\"./data/real/bcsstk02.mtx\" M1_WITH_MNC=\"./data/real/withmnc/bcsstk02.csv\" M2_WITHOUT_MNC=\"./data/real/dwt__66.mtx\" M2_WITH_MNC=\"./data/real/withmnc/dwt__66.csv\"
+bin/daphne scripts/experiment/matmul_w.daph M1_WITHOUT_MNC=\"./data/real/bp__1000.mtx\" M1_WITH_MNC=\"./data/real/withmnc/bp__1000.csv\" M2_WITHOUT_MNC=\"./data/real/bp__1200.mtx\" M2_WITH_MNC=\"./data/real/withmnc/bp__1200.csv\"
+bin/daphne scripts/experiment/matmul_w.daph M1_WITHOUT_MNC=\"./data/real/G10.mtx\" M1_WITH_MNC=\"./data/real/withmnc/G10.csv\" M2_WITHOUT_MNC=\"./data/real/G11.mtx\" M2_WITH_MNC=\"./data/real/withmnc/G11.csv\"
+bin/daphne scripts/experiment/matmul_w.daph M1_WITHOUT_MNC=\"./data/real/G14.mtx\" M1_WITH_MNC=\"./data/real/withmnc/G14.csv\" M2_WITHOUT_MNC=\"./data/real/G10.mtx\" M2_WITH_MNC=\"./data/real/withmnc/G10.csv\"
+bin/daphne scripts/experiment/matmul_w.daph M1_WITHOUT_MNC=\"./data/real/G11.mtx\" M1_WITH_MNC=\"./data/real/withmnc/G11.csv\" M2_WITHOUT_MNC=\"./data/real/G14.mtx\" M2_WITH_MNC=\"./data/real/withmnc/G14.csv\"
+bin/daphne scripts/experiment/matmul_w.daph M1_WITHOUT_MNC=\"./data/real/G22.mtx\" M1_WITH_MNC=\"./data/real/withmnc/G22.csv\" M2_WITHOUT_MNC=\"./data/real/G27.mtx\" M2_WITH_MNC=\"./data/real/withmnc/G27.csv\"
+bin/daphne scripts/experiment/matmul_w.daph M1_WITHOUT_MNC=\"./data/real/gre__115.mtx\" M1_WITH_MNC=\"./data/real/withmnc/gre__115.csv\" M2_WITHOUT_MNC=\"./data/real/football__115.mtx\" M2_WITH_MNC=\"./data/real/withmnc/football__115.csv\"
+bin/daphne scripts/experiment/matmul_w.daph M1_WITHOUT_MNC=\"./data/real/lp_scagr7.mtx\" M1_WITH_MNC=\"./data/real/withmnc/lp_scagr7.csv\" M2_WITHOUT_MNC=\"./data/real/gre__185.mtx\" M2_WITH_MNC=\"./data/real/withmnc/gre__185.csv\"
+bin/daphne scripts/experiment/matmul_w.daph M1_WITHOUT_MNC=\"./data/real/gre__512.mtx\" M1_WITH_MNC=\"./data/real/withmnc/gre__512.csv\" M2_WITHOUT_MNC=\"./data/real/dw256A.mtx\" M2_WITH_MNC=\"./data/real/withmnc/dw256A.csv\"
+bin/daphne scripts/experiment/matmul_w.daph M1_WITHOUT_MNC=\"./data/real/str__200.mtx\" M1_WITH_MNC=\"./data/real/withmnc/str__200.csv\" M2_WITHOUT_MNC=\"./data/real/str__400.mtx\" M2_WITH_MNC=\"./data/real/withmnc/str__400.csv\"
--- scripts/experiment/run_write_mnc_md.sh
+++ scripts/experiment/run_write_mnc_md.sh
@@ -15,7 +15,10 @@ bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/w/w_363_
 
 
 # write metadata for real matrices
-# all datasets: bcsstk02 (66,66), bp__1000 (822,822), bp_1200 (822,822), football (35,35), G10 (800,800), G11 (800,800), G14(800,800), G22 (2000,2000), G27 (2000,2000), gre__115 (115,115), gre__185 (185,185), gre__343 (343,343), gre__512 (512,512), str__200 (363,363), str__400 (363,363)
+# all datasets: bcsstk02 (66,66), bp__1000 (822,822), bp_1200 (822,822), football (35,35), G10 (800,800), 
+# G11 (800,800), G14(800,800), G22 (2000,2000), G27 (2000,2000), gre__115 (115,115), gre__185 (185,185), 
+# gre__343 (343,343), gre__512 (512,512), str__200 (363,363), str__400 (363,363), lp_scagr7 (129, 185), 
+# football__115 (115,115), dw256A (512,512), dwt__66 (66,66)
 bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/bcsstk02.mtx\" M1_O=\"./data/real/withmnc/bcsstk02.csv\"
 bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/bp__1000.mtx\" M1_O=\"./data/real/withmnc/bp__1000.csv\"
 bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/bp__1200.mtx\" M1_O=\"./data/real/withmnc/bp__1200.csv\"
@@ -31,3 +34,7 @@ bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/gre
 bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/gre__512.mtx\" M1_O=\"./data/real/withmnc/gre__512.csv\"
 bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/str__200.mtx\" M1_O=\"./data/real/withmnc/str__200.csv\"
 bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/str__400.mtx\" M1_O=\"./data/real/withmnc/str__400.csv\"
+bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/lp_scagr7.mtx\" M1_O=\"./data/real/withmnc/lp_scagr7.csv\"
+bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/football__115.mtx\" M1_O=\"./data/real/withmnc/football__115.csv\"
+bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/dw256A.mtx\" M1_O=\"./data/real/withmnc/dw256A.csv\"
+bin/daphne scripts/experiment/write_mnc_md.daph M1_WITHOUT_MNC=\"./data/real/dwt__66.mtx\" M1_O=\"./data/real/withmnc/dwt__66.csv\"
